d823fa3d693f4aa3fdfce3227703ec5db4cf1747
zulimazuli <zulimazuli@gmail.com>
2021-03-23 00:17:56 +0100

    language switcher refactor

--- src/components/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/LanguageSwitcher/LanguageSwitcher.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import i18n from '../../i18n';
 
 import styles from './LanguageSwitcher.module.css';
 
@@ -6,10 +7,15 @@ import globeIcon from '../../assets/images/icon-globe.png';
 import polishIcon from '../../assets/images/icon-polish.png';
 
 const LanguageSwitcher = () => {
+    const handleSwitch = (lang) => {
+        i18n.changeLanguage(lang)
+        window.location.reload();
+    }
+
     return (
         <div className={styles.LanguageSwitcher} title="Change Language">
-            <a href="?lang=en"><img className={styles.LangIcon} src={globeIcon} title="English" alt="EN" /></a>
-            <a href="?lang=pl"><img className={styles.LangIcon} src={polishIcon} title="Polish" alt="PL" /></a>
+            <img onClick={() => handleSwitch('en')} className={styles.LangIcon} src={globeIcon} title="English" alt="EN" />
+            <img onClick={() => handleSwitch('pl')} className={styles.LangIcon} src={polishIcon} title="Polish" alt="PL" />
         </div>
     );
 }
